4c415e361114c0e48a6f3b77485f2dbbdd0edf3c
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2018-10-22 23:55:01 +0200

    Updates

---
+++ precache-manifest.872b20d1ba6562ef823c5394ca914cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoroTimer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "129256dc01a3188116f5",
+    "url": "/pomodoroTimer/static/js/runtime~main.129256dc.js"
+  },
+  {
+    "revision": "4c6352132bda9235a749",
+    "url": "/pomodoroTimer/static/js/main.4c635213.chunk.js"
+  },
+  {
+    "revision": "38ef84dfedc6864844be",
+    "url": "/pomodoroTimer/static/js/1.38ef84df.chunk.js"
+  },
+  {
+    "revision": "4c6352132bda9235a749",
+    "url": "/pomodoroTimer/static/css/main.892f2723.chunk.css"
+  },
+  {
+    "revision": "4e37f17276a5605efdf243cdd20197a2",
+    "url": "/pomodoroTimer/index.html"
+  }
+];
