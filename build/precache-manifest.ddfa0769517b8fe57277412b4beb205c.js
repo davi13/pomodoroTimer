ec26798fdd192a9da808f35b2510c6109d4af33d
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2018-10-23 09:37:02 +0200

    resolution bug git pages

---
+++ build/precache-manifest.ddfa0769517b8fe57277412b4beb205c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoroTimer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "129256dc01a3188116f5",
+    "url": "/pomodoroTimer/static/js/runtime~main.129256dc.js"
+  },
+  {
+    "revision": "4c6352132bda9235a749",
+    "url": "/pomodoroTimer/static/js/main.4c635213.chunk.js"
+  },
+  {
+    "revision": "38ef84dfedc6864844be",
+    "url": "/pomodoroTimer/static/js/1.38ef84df.chunk.js"
+  },
+  {
+    "revision": "4c6352132bda9235a749",
+    "url": "/pomodoroTimer/static/css/main.892f2723.chunk.css"
+  },
+  {
+    "revision": "2c9cfb5b6e298060b0407053875ee491",
+    "url": "/pomodoroTimer/index.html"
+  }
+];
